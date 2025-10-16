67e8cb3362343fe122a1527cd84fa56c05f1b048
sandeep yadav <sandeepyada234abc@gmail.com>
2025-10-16 17:43:53 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import './App.css';
+import AboutPage from "./landing_page/about/AboutPage";
+import HomePage from "./landing_page/home/HomePage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import ProductPage from "./landing_page/products/ProductsPage";
+import SignIn from "./landing_page/signup/SignIn";
+import SignUp from "./landing_page/signup/SignUp";
+import SupportPage from "./landing_page/support/SupportPage";
+
+import Footer from "./landing_page/Footer";
+import Navbar from "./landing_page/Navbar";
+import NotFound from "./landing_page/NotFound";
+
+function App() {
+  return (
+    <div className="App">
+    
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/landing_page/products/LeftSection.js
@@ -0,0 +1,43 @@
+
+function LeftSection({
+  imageURL,
+  productName,
+  productDesription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-lg-6">
+          <img src={imageURL} alt="product-img"  style={{width: "100%"}}/>
+        </div>
+        <div className="col-lg-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+          <div>
+            <a href={tryDemo}>Try Demo</a>
+            <a href={learnMore} style={{ marginLeft: "50px" }}>
+              Learn More
+            </a>
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img src="media/images/googlePlayBadge.svg" alt="product-img"/>
+            </a>
+            <a href={appStore}>
+              <img
+                src="media/images/appstoreBadge.svg" alt="product-img"
+                style={{ marginLeft: "50px" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftSection;
